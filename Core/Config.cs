6cc1c02dfb581817654706d9463e1fe3df8c2dd6
I54m <I54m@I54m.com>
2025-06-25 11:28:19 +1200

    Added Basic settings Wrapper with setters for bools

--- Core/Config.cs
+++ Core/Config.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace DocuSync.Core
 {
-    internal class Config
+    public static class Config
     {
+        public static string LocalPath => Environment.ExpandEnvironmentVariables(Properties.Settings.Default.LocalPath ?? "");
+        public static string RemotePath => Environment.ExpandEnvironmentVariables(Properties.Settings.Default.RemotePath ?? "");
+        public static IReadOnlyList<String> ExcludedFolders => Properties.Settings.Default.ExcludedFolders?.Cast<string>().ToList() ?? new List<string>();
+        public static IReadOnlyList<String> ExcludedFiles => Properties.Settings.Default.ExcludedFiles?.Cast<string>().ToList() ?? new List<string>();
+        public static bool DryRun
+        {
+            get => Properties.Settings.Default.DryRun;
+            set
+            {
+                Properties.Settings.Default.DryRun = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        public static bool PromptOnConflict
+        {
+            get => Properties.Settings.Default.PromptOnConflict;
+            set
+            {
+                Properties.Settings.Default.PromptOnConflict = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        public static bool StartMinimized
+        {
+            get => Properties.Settings.Default.StartMinimized;
+            set
+            {
+                Properties.Settings.Default.StartMinimized = value;
+                Properties.Settings.Default.Save();
+            }
+        }
     }
 }
