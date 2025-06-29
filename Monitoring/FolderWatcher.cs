7c3b82cc45c560661050c3ded552450679ffc5ca
I54m <I54m@I54m.com>
2025-06-29 14:52:35 +1200

    Added explanation comment for FolderWatcher

--- Monitoring/FolderWatcher.cs
+++ Monitoring/FolderWatcher.cs
@@ -8,5 +8,8 @@ namespace DocuSync.Monitoring
 {
     internal class FolderWatcher
     {
+        //monitor local TLD for changes if something is added then make it into a symlink
+        //if an excluded folder/file is updated then copy the updated version to the remote
+        //if an excluded folder/file is updated on the remote then we copy the new version to local
     }
 }
